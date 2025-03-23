88159c94f832bc26cda72c92d5d66d0c03e51b01
Mirsardor <86027153+mirsardor@users.noreply.github.com>
2025-03-23 18:56:45 +0300

    6hw

---
+++ trainer_ui_swing/src/main/java/ru/spbu/mirsardor/gui/config/SpringConfig.java
@@ -0,0 +1,15 @@
+package ru.spbu.mirsardor.gui.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import ru.spbu.mirsardor.spring.jdbc.config.DbConfig;
+
+@Configuration
+@Import(DbConfig.class)
+@ComponentScan(basePackages = "ru.spbu.mirsardor")
+@PropertySource("jdbc.properties")
+public class SpringConfig {
+
+}
