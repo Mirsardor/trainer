8fb3a5abf269a00b4befa349921ebeda86908fc2
Mirsardor <86027153+mirsardor@users.noreply.github.com>
2025-04-05 22:59:45 +0500

    9-10hw

---
+++ trainer_api/src/test/java/ru/spbu/mirsardor/api/controller/QuestionControllerTest.java
@@ -0,0 +1,35 @@
+package ru.spbu.mirsardor.api.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import ru.spbu.mirsardor.api.dto.OpenQuestionCardDto;
+import ru.spbu.mirsardor.domain.model.OpenQuestionCard;
+import ru.spbu.mirsardor.domain.repo.QuestionRepository;
+
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class QuestionControllerTest {
+    @MockitoBean
+    private QuestionRepository taskRepository;
+
+    @Autowired
+    private QuestionController controller;
+
+    @Test
+    @DisplayName("Создание Task с корректными code и title проходит успешно")
+    void having_task_when_list_then_return() {
+        Mockito.when(taskRepository.findAll())
+                .thenReturn(List.of(new OpenQuestionCard(1L, "Задача 1", "123")));
+        List<OpenQuestionCardDto> list = controller.list();
+        Assertions.assertEquals(1, list.size());
+    }
+}
